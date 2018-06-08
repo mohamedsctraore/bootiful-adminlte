25160b86ae1afaa2e3cef08aaee547bcbcedb2ba
yinguowei <yinguowei@gmail.com>
2018-06-08 23:56:57 +0800

    first init.

---
+++ src/main/java/cn/yinguowei/BootifulAdminlteApplication.java
@@ -0,0 +1,12 @@
+package cn.yinguowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulAdminlteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulAdminlteApplication.class, args);
+	}
+}
