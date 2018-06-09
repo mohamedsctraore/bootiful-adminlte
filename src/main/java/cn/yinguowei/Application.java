bebb3f64f58f960060a74c3dd437847fe3fc8968
yinguowei <yinguowei@gmail.com>
2018-06-09 19:15:05 +0800

    add layout.

--- src/main/java/cn/yinguowei/Application.java
+++ src/main/java/cn/yinguowei/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,25 +19,20 @@ public class Application {
 }
 
 @Controller
-class HelloController {
-    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+class AdminController {
+    private static Logger logger = LoggerFactory.getLogger(AdminController.class);
 
-    @GetMapping("/")
-    public String hello() {
+//    @GetMapping(value = {"/", ""})
+//    @PostMapping(value = {"/", ""})
+    @RequestMapping("/")
+    public String home() {
         return "redirect:index2.html";
     }
 
-    @GetMapping(value = {"/**/*.html"})
+    @GetMapping("/**/*.html")
     public String route(HttpServletRequest request) {
-//        logger.debug("DynamicController.route: request.getRequestURI() = {}", request.getRequestURI());
+        logger.debug("AdminController.route: request.getRequestURI() = {}", request.getRequestURI());
         String path = request.getRequestURI();
-//        if (path.startsWith("/")) {
-//            System.out.println("path.substring(1, path.length() - 5) = " + path.substring(1, path.length() - 5));
-//            return path.substring(1, path.length() - 5); // remove ".html"
         return path;
-//        } else {
-//            System.out.println("path.substring(0, path.length() - 5) = " + path.substring(0, path.length() - 5));
-//            return path.substring(0, path.length() - 5);
-//        }
     }
 }
